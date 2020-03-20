fcfa95e6560b6d8202ad95c58f8977744e8b2ada
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-03-20 21:43:53 +0600

    styling by javaScript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 //how to select by id
 console.log(document.getElementById('task-title').id);
 console.log(document.getElementById('task-title').className); //Note::when you will select class by id then you have to use className
+
+
+
+// styling by javascript
+console.log(document.getElementById('task-title').style.background='#2e2e2e');
+console.log(document.getElementById('task-title').style.color='#fff');
+console.log(document.getElementById('task-title').style.marginTop='10px');
+console.log(document.getElementById('task-title').style.padding='5px');
+ 
+
