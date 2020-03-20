5a69bab906c2631b9aad3240be68811e249a692e
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-03-20 21:50:16 +0600

    how to changne inner content of element

--- js/app.js
+++ js/app.js
@@ -11,3 +11,9 @@ console.log(document.getElementById('task-title').style.marginTop='10px');
 console.log(document.getElementById('task-title').style.padding='5px');
  
 
+
+// selecting text of tags by three way 
+console.log(document.getElementById('task-title').textContent='Below are all of list item');
+console.log(document.getElementById('task-title').innerText='All list item are below');
+console.log(document.getElementById('task-title').innerHTML='<span>All list item are below </span>');
+
